78d3df263bd3885a5c5edfe87a53fc95a6f554e6
Daaniel-Sans <danpavtel@gmail.com>
2026-04-10 19:20:57 +0200

    feat: implement update and delete operations in ClienteDAO
    - Add actualizar method to ClienteDAO using PreparedStatement
    - Add borrar method to ClienteDAO to remove records by ID
    - Update Main
    - Ensure database remains clean after execution

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,22 @@ import pojo.Cliente;
 public class Main {
     public static void main (String [] args) {
         ClienteDAO clienteDAO = new ClienteDAO();
-        Cliente a = new Cliente("Ramiro", "ramiroramirez@gmail.com", "999888777", 82, 20.50, 2);
+        clienteDAO.borrar(11);
 
-       clienteDAO.insertarCliente(a);
-       System.out.println("✅ Cliente insertado correctamente.\n");
+        System.out.println("\n Insertar cliente");
+        Cliente a = new Cliente("Ramiro Ramirez", "ramiroramirez@gmail.com", "999888777", 82, 20.50, 2);
+        clienteDAO.insertarCliente(a);
+        System.out.println("✅ Cliente insertado correctamente con ID: " + a.getId());
+
+        a.setEdad(83);
+        a.setDineroGastado(120.75);
+        a.setProductosComprados(3);
+        System.out.println("Actualización del cliente con ID " + a.getId() + "...");
+        clienteDAO.actualizar(a);
+
+        System.out.println("Borrando al cliente con ID " + a.getId());
+        clienteDAO.borrar(a.getId());
+
+        System.out.println("\n✅ Cliente borrado. Esto no ha pasado... 🥷");
     }
 }
--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -4,14 +4,16 @@ import pojo.Cliente;
 
 import java.sql.*;
 
+import static java.sql.DriverManager.getConnection;
+
 public class ClienteDAO {
     private String url = "jdbc:sqlite:prueba.sqlite3";
 
     public void insertarCliente(Cliente a) {
 
-        try (Connection conn = DriverManager.getConnection(url)) {
+        try (Connection conn = getConnection(url)) {
             String sql = "INSERT INTO clientes (nombre, email, telefono, edad, dinero_gastado, productos_comprados) VALUES (?,?,?,?,?,?)";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+            PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             pstmt.setString(1, a.getNombre());
             pstmt.setString(2, a.getEmail());
             pstmt.setString(3, a.getTelefono());
@@ -29,6 +31,45 @@ public class ClienteDAO {
         }catch (SQLException e) {
             System.out.println("Error 😿" + e.getMessage());
         }
-
     }
+
+    public void actualizar(Cliente cliente) {
+        String sql = "UPDATE clientes SET nombre=?, email=?, telefono=?, edad=?, " +
+                "dinero_gastado=?, productos_comprados=? WHERE id=?";
+        try (Connection con = getConnection(url);
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+            pstmt.setString(1, cliente.getNombre());
+            pstmt.setString(2, cliente.getEmail());
+            pstmt.setString(3, cliente.getTelefono());
+            pstmt.setInt(4, cliente.getEdad());
+            pstmt.setDouble(5, cliente.getDineroGastado());
+            pstmt.setInt(6, cliente.getProductosComprados());
+            pstmt.setInt(7, cliente.getId());
+            int filas = pstmt.executeUpdate();
+            System.out.println("Clientes actualizados: " + filas);
+
+        } catch (SQLException e) {
+            System.err.println("Vaaaaya hombre... Hubo un error al actualizar cliente: " + e.getMessage() + " 😿");
+        }
+    }
+
+    public void borrar(int id) {
+        String sql = "DELETE FROM clientes WHERE id=?";
+        try (Connection con = getConnection(url);
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+            pstmt.setInt(1, id);
+            int filas = pstmt.executeUpdate();
+            System.out.println("Clientes borrados: " + filas);
+
+        } catch (SQLException e) {
+            System.err.println("uUuUups... Error al borrar cliente: " + e.getMessage() + " 😿");
+        }
+    }
+
+
+
 }
+
+
